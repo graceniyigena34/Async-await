7269645d4c986c80914d83571ed58746a18ba2d5
graceniyigena34 <graceniyigena34@gmail.com>
2025-10-03 12:48:30 +0200

    I added the example of async and await

--- index.js
+++ index.js
@@ -4,36 +4,78 @@
 
 // Promise  
 
-async function fetchData(userId){
-    try{
-          //1. make an Api request
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId} `)
-      //2. check if the request is successful
+// async function fetchData(userId){
+//     try{
+//           //1. make an Api request
+//         const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId} `)
+//       //2. check if the request is successful
 
-      if(!response.ok) {
-        throw new Error(`HTTP error! status:${response.status}`)
-      }
+//       if(!response.ok) {
+//         throw new Error(`HTTP error! status:${response.status}`)
+//       }
 
-      //parse the json response
-      const data = await response.json();
-      return data 
+//       //parse the json response
+//       const data = await response.json();
+//       return data 
     
-}catch (error) {
-    console.log("error fetching user data", error)
-    return null
+// }catch (error) {
+//     console.log("error fetching user data", error)
+//     return null
 
+//     }
+// }
+// async function displayuser(){
+//     const user = await fetchData(1) 
+//     if (user) {
+//         console.log("Name:", user.name);
+//          console.log("User email:", user.email);
+
+//     } else {
+//         console.log("user not found");
+//     }
+// }
+
+// displayuser()
+
+
+
+//Question 1
+async function fetchAuthorsSithArticles(){
+    const response=await fetch("https://jsonplaceholder.typicode.com/posts?_embed=comments&_expand=user");
+    const posts=await response.json();
+    const author={};
+    posts.forEach(post=>{
+        const userId=post.user.id;
+        author[userId]={
+            id:post.user.id,
+            name:post.user.name,
+            articles:[]
+        }
+author[userId].articles.push({
+            id:post.id,
+            title:post.title,
+            body:post.body,
+            comments:post.comments.map(c=>({
+                id:c.id,
+                content:c.body
+            })),
+});
     }
+)
+const authorsArray=Object.values(author);
+return authorsArray;
 }
-async function displayuser(){
-    const user = await fetchData(1) 
-    if (user) {
-        console.log("Name:", user.name);
-         console.log("User email:", user.email);
+fetchAuthorsSithArticles().then(authors=>{
+    console.log(JSON.stringify(authors,null,2));
+});
 
-    } else {
-        console.log("user not found");
-    }
-}
 
-displayuser()
+
+
+
+
+
+
+
+
 
